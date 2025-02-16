ca300638b5b76f6392904cba1f42d0eea9e196ef
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-02-16 12:23:18 +0100

    Add icon support to duration fields

--- panel/views/fields/duration.php
+++ panel/views/fields/duration.php
@@ -1,17 +1,20 @@
 <?php $this->layout('fields.field') ?>
-<input <?= $this->attr([
-            'class'          => 'form-input',
-            'type'           => 'number',
-            'id'             => $field->name(),
-            'name'           => $field->formName(),
-            'min'            => $field->get('min'),
-            'max'            => $field->get('max'),
-            'step'           => $field->get('step'),
-            'value'          => $field->value(),
-            'required'       => $field->isRequired(),
-            'disabled'       => $field->isDisabled(),
-            'hidden'         => $field->isHidden(),
-            'data-field'     => 'duration',
-            'data-unit'      => $field->get('unit', 'seconds'),
-            'data-intervals' => $field->has('intervals') ? implode(', ', $field->get('intervals')) : null,
-        ]) ?>>
+<div class="form-input-wrap">
+    <span class="form-input-icon"><?= $this->icon($field->get('icon', 'hourglass')) ?></span>
+    <input <?= $this->attr([
+                'class'          => 'form-input',
+                'type'           => 'number',
+                'id'             => $field->name(),
+                'name'           => $field->formName(),
+                'min'            => $field->get('min'),
+                'max'            => $field->get('max'),
+                'step'           => $field->get('step'),
+                'value'          => $field->value(),
+                'required'       => $field->isRequired(),
+                'disabled'       => $field->isDisabled(),
+                'hidden'         => $field->isHidden(),
+                'data-field'     => 'duration',
+                'data-unit'      => $field->get('unit', 'seconds'),
+                'data-intervals' => $field->has('intervals') ? implode(', ', $field->get('intervals')) : null,
+            ]) ?>>
+</div>

d9f0c1feb3b9855d5bdc8bb189c0aaab2792e7ca
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-02-28 22:31:08 +0100

    Properly validate select fields

--- formwork/fields/select.php
+++ formwork/fields/select.php
@@ -1,8 +1,10 @@
 <?php
 
 use Formwork\Cms\App;
+use Formwork\Fields\Exceptions\ValidationException;
 use Formwork\Fields\Field;
 use Formwork\Utils\Arr;
+use Formwork\Utils\Constraint;
 
 return function (App $app) {
     return [
@@ -11,6 +13,14 @@ return function (App $app) {
         },
 
         'validate' => function (Field $field, $value) {
+            if (Constraint::isEmpty($value)) {
+                return '';
+            }
+
+            if (!array_key_exists($value, $field->options())) {
+                throw new ValidationException(sprintf('Invalid value for field "%s" of type "%s"', $field->name(), $field->type()));
+            }
+
             if (is_numeric($value)) {
                 // This reliably casts numeric values to int or float
                 return $value + 0;

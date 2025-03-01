c4cbb1559ed82ad1acdcb8e2ed0d503c2f2f1033
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-03-01 18:40:33 +0100

    Fix login notification

--- panel/views/partials/login/notification.php
+++ panel/views/partials/login/notification.php
@@ -1,7 +1,11 @@
 <?php if ($notification = $panel->notifications()) : ?>
     <?php if (($type = $notification[0]['type']) === 'error') : ?>
-        <div class="login-modal-danger"><?= $this->icon($notification[0]['icon']) ?> <?= $this->escape($notification[0]['text']) ?></div>
+        <div class="login-modal-danger"><?= $this->icon('exclamation-octagon') ?> <?= $this->escape($notification[0]['text']) ?></div>
+    <?php elseif ($type === 'warning') : ?>
+        <div class="login-modal-warning"><?= $this->icon('exclamation-triangle') ?> <?= $this->escape($notification[0]['text']) ?></div>
+    <?php elseif ($type === 'success') : ?>
+        <div class="login-modal-success"><?= $this->icon('check-circle') ?> <?= $this->escape($notification[0]['text']) ?></div>
     <?php else : ?>
-        <div class="login-modal-<?= $type ?>"><?= $this->icon($notification[0]['icon']) ?> <?= $this->escape($notification[0]['text']) ?></div>
+        <div class="login-modal-info"><?= $this->icon('info-circle') ?> <?= $this->escape($notification[0]['text']) ?></div>
     <?php endif ?>
 <?php endif ?>

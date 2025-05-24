9a6b0937eba4d262980fe4893d37191828889e28
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-05-24 17:13:24 +0200

    Restrict uploaded files destinations

--- formwork/src/Files/Services/FileUploader.php
+++ formwork/src/Files/Services/FileUploader.php
@@ -12,6 +12,7 @@ use Formwork\Sanitizer\SvgSanitizer;
 use Formwork\Utils\Arr;
 use Formwork\Utils\FileSystem;
 use Formwork\Utils\MimeType;
+use Formwork\Utils\Path;
 use Formwork\Utils\Str;
 
 class FileUploader
@@ -21,11 +22,17 @@ class FileUploader
      */
     protected array $allowedMimeTypes;
 
+    /**
+     * @var array<string>
+     */
+    protected array $baseDestinations;
+
     public function __construct(
         protected Config $config,
         protected FileFactory $fileFactory,
     ) {
         $this->allowedMimeTypes = Arr::map($this->config->get('system.files.allowedExtensions'), fn(string $ext) => MimeType::fromExtension($ext));
+        $this->baseDestinations = $this->config->get('system.files.uploads.baseDestinations');
     }
 
     /**
@@ -51,6 +58,12 @@ class FileUploader
 
         if (!in_array($mimeType, $allowedMimeTypes, true)) {
             throw new TranslatedException(sprintf('Invalid mime type %s for file uploads', $mimeType), 'upload.error.mimeType');
+        }
+
+        $resolvedPath = FileSystem::resolvePath($destinationPath);
+
+        if (!Arr::some($this->baseDestinations, fn(string $basePath) => Path::isRelativeTo($resolvedPath, FileSystem::resolvePath($basePath)))) {
+            throw new TranslatedException('Invalid destination path', 'upload.error.cannotMoveToDestination');
         }
 
         $filename = Str::slug($name ?? pathinfo($uploadedFile->clientName(), PATHINFO_FILENAME)) . '.' . MimeType::toExtension($mimeType);

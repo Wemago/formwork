98356cdfb2df584202fe747cf61e5bc09cb64f6b
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-06-28 22:49:00 +0200

    Keep uniform logic to filter stack frames

--- panel/views/errors/error.php
+++ panel/views/errors/error.php
@@ -33,7 +33,7 @@
                         <?= Formwork\Debug\CodeDumper::dumpLine($throwable->getFile(), $throwable->getLine(), $app->config()->get('system.debug.contextLines', 5)) ?>
                     </details>
                     <?php foreach ($throwable->getTrace() as $frame) : ?>
-                        <?php if (isset($frame['file'], $frame['line']) && $frame['file'] !== $throwable->getFile() && $frame['line'] !== $throwable->getLine()) : ?>
+                        <?php if (isset($frame['file'], $frame['line']) && ($frame['file'] !== $throwable->getFile() || $frame['line'] !== $throwable->getLine())): ?>
                             <details>
                                 <summary><a class="error-debug-editor-uri" href="<?= Formwork\Utils\Str::interpolate($app->config()->get('system.debug.editorUri'), ['filename' => $frame['file'], 'line' => $frame['line']]) ?>"><span class="error-debug-filename"><?= preg_replace('/([^\/]+)$/', '<strong>$1</strong>', $frame['file']) ?></span><span class="error-debug-line">:<?= $frame['line'] ?></span></a></summary>
                                 <?= Formwork\Debug\CodeDumper::dumpBacktraceFrame($frame, $app->config()->get('system.debug.contextLines', 5)) ?>

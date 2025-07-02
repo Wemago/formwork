cd7dd32326c69e95f62cf78022705f0761340924
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-07-02 21:36:56 +0200

    Store named sections state to `window.localStorage`

--- panel/src/ts/components/sections.ts
+++ panel/src/ts/components/sections.ts
@@ -3,9 +3,23 @@ import { $$ } from "../utils/selectors";
 export class Sections {
     constructor() {
         $$(".collapsible .section-header").forEach((element) => {
+            const section = element.parentNode as HTMLElement;
+
+            const formName = section.closest("form")?.dataset.form;
+            const key = formName ? `${formName}.${section.id}` : section.id;
+
+            if (section.id) {
+                const state = window.localStorage.getItem(`formwork.sectionStatus[${key}]`);
+                if (state) {
+                    section.classList.toggle("collapsed", state === "collapsed");
+                }
+            }
+
             element.addEventListener("click", () => {
-                const section = element.parentNode as HTMLElement;
-                section.classList.toggle("collapsed");
+                const collapsed = section.classList.toggle("collapsed");
+                if (section.id) {
+                    window.localStorage.setItem(`formwork.sectionStatus[${key}]`, collapsed ? "collapsed" : "expanded");
+                }
             });
         });
     }

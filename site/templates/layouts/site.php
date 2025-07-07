68e461ffb2d2c3694c483f8751540446c1ca892b
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-07-07 23:24:07 +0200

    Review templates

--- site/templates/layouts/site.php
+++ site/templates/layouts/site.php
@@ -6,16 +6,20 @@
     <?= $this->insert('_meta') ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚧</text></svg>">
-    <link rel="stylesheet" type="text/css" href="<?= $this->assets()->get('css/style.min.css')->uri() ?>">
-    <script src="<?= $this->assets()->get('js/script.min.js')->uri() ?>"></script>
+    <link rel="stylesheet" type="text/css" href="<?= $this->assets()->get('css/style.css')->uri(includeVersion: true) ?>">
+    <script src="<?= $this->assets()->get('js/script.js')->uri(includeVersion: true) ?>"></script>
 </head>
 
 <body>
-    <?= $this->insert('_menu') ?>
+    <header class="header">
+        <div class="container">
+            <?= $this->insert('_menu') ?>
+        </div>
+    </header>
     <?= $this->insert('_cover-image') ?>
     <?= $this->content() ?>
-    <footer>
-        <div class="container small">
+    <footer class="footer">
+        <div class="container">
             &copy; 2017-2025 &mdash; Made with <a href="https://github.com/getformwork/formwork">Formwork</a>
         </div>
     </footer>

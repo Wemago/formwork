64910e76ba74d51ed3f611f2d865b817b27c0e4a
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-08-07 15:55:06 +0200

    Clear cache even if not enabled

--- formwork/src/Panel/Controllers/CacheController.php
+++ formwork/src/Panel/Controllers/CacheController.php
@@ -42,11 +42,9 @@ final class CacheController extends AbstractController
      */
     private function clearPagesCache(AbstractCache $cache): void
     {
-        if ($this->config->get('system.cache.enabled')) {
-            $cache->clear();
-            if ($this->site->contentPath() !== null) {
-                FileSystem::touch($this->site->contentPath());
-            }
+        $cache->clear();
+        if ($this->site->contentPath() !== null) {
+            FileSystem::touch($this->site->contentPath());
         }
     }
 

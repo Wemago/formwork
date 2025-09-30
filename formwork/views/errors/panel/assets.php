de5dcbf40ef0d88dee93cd9957d9e4af4cbe8cf8
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-09-30 23:42:45 +0200

    Use pnpm instead of yarn

--- formwork/views/errors/panel/assets.php
+++ formwork/views/errors/panel/assets.php
@@ -1,4 +1,4 @@
 <?php include dirname(__DIR__) . '/partials/header.php' ?>
 <h2>The administration panel is currently offline due to technical problems</h2>
-<p>Required panel assets were not found. If you are the maintainer of this site, please run <code>cd panel; yarn install && yarn build</code></p>
+<p>Required panel assets were not found. If you are the maintainer of this site, please run <code>cd panel; pnpm install && pnpm build</code></p>
 <?php include dirname(__DIR__) . '/partials/footer.php' ?>

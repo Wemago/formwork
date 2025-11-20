9689e24c99745378302e20541a4aead7bcbd13d6
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-11-20 19:40:10 +0100

    Use section name instead of id

--- panel/views/fields/layout/sections.php
+++ panel/views/fields/layout/sections.php
@@ -5,8 +5,8 @@
 <?php foreach ($layout->sections()->groupBy('tab', $layout->tabs()->first()?->name()) as $tabName => $sections): ?>
     <?php $this->define('sections') ?>
     <div class="sections">
-        <?php foreach ($sections as $id => $section) : ?>
-            <section class="<?= $this->classes(['section',  'collapsible' => $section->is('collapsible'), 'collapsed' => $section->is('collapsed')]) ?>" id="section-<?= $this->escapeAttr($id) ?>">
+        <?php foreach ($sections as $section) : ?>
+            <section class="<?= $this->classes(['section',  'collapsible' => $section->is('collapsible'), 'collapsed' => $section->is('collapsed')]) ?>" id="section-<?= $this->escapeAttr($section->name()) ?>">
                 <div class="section-header">
                     <?php if ($section->is('collapsible')) : ?>
                         <button type="button" class="button section-toggle mr-2" title="<?= $this->translate('panel.sections.toggle') ?>" aria-label="<?= $this->translate('panel.sections.toggle') ?>"><?= $this->icon('chevron-up') ?></button>

40609befc495e4a5661e405ac16d4f24c2c8c523
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-11-22 22:09:26 +0100

    Ensure panel app uses cache

--- formwork/src/Panel/Controllers/AssetsController.php
+++ formwork/src/Panel/Controllers/AssetsController.php
@@ -6,6 +6,7 @@ use Formwork\Http\FileResponse;
 use Formwork\Http\Response;
 use Formwork\Router\RouteParams;
 use Formwork\Utils\FileSystem;
+use Formwork\Utils\Str;
 
 final class AssetsController extends AbstractController
 {
@@ -17,7 +18,13 @@ final class AssetsController extends AbstractController
         $path = FileSystem::joinPaths($this->config->get('system.panel.paths.assets'), $routeParams->get('type'), $routeParams->get('file'));
 
         if (FileSystem::isFile($path, assertExists: false)) {
-            $headers = ($this->request->query()->has('v') || $routeParams->get('type') === 'icons')
+            $headers = (
+                $this->request->query()->has('v')
+                || $routeParams->get('type') === 'icons'
+
+                // Panel js chunks contain an hash in their filename, they can be cached with immutability
+                || ($routeParams->get('type') === 'js' && Str::startsWith($routeParams->get('file'), 'chunks/'))
+            )
                 ? ['Cache-Control' => 'private, max-age=31536000, immutable']
                 : [];
             return new FileResponse($path, headers: $headers, autoEtag: true, autoLastModified: true);
--- panel/views/partials/scripts.php
+++ panel/views/partials/scripts.php
@@ -4,7 +4,7 @@
 
 <?php if ($this->assets()->has('js/app.min.js')): ?>
     <script type="module">
-        import { app } from "<?= $this->assets()->get('js/app.min.js')->uri() ?>";
+        import { app } from "<?= $this->assets()->get('js/app.min.js')->uri(includeVersion: true) ?>";
         app.load(<?= Formwork\Parsers\Json::encode($panel->getAppConfig()) ?>);
     </script>
 <?php endif ?>

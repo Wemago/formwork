bf7f7edab0f4e4db4b88a78a6139eb25ad45b60f
Giuseppe Criscione <18699708+giuscris@users.noreply.github.com>
2025-11-30 11:04:59 +0100

    Add a controller to serve plugin assets

---
+++ formwork/src/Plugins/Controllers/AssetsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Formwork\Plugins\Controllers;
+
+use Formwork\Controllers\AbstractController;
+use Formwork\Controllers\PageController;
+use Formwork\Http\FileResponse;
+use Formwork\Http\Response;
+use Formwork\Plugins\Plugin;
+use Formwork\Router\RouteParams;
+use Formwork\Services\Container;
+use Formwork\Utils\FileSystem;
+
+final class AssetsController extends AbstractController
+{
+    public function __construct(
+        private Container $container,
+        private Plugin $plugin
+    ) {
+        $this->container->call(parent::__construct(...));
+    }
+
+    /**
+     * AssetsController@asset action
+     */
+    public function asset(RouteParams $routeParams): Response
+    {
+        $path = FileSystem::joinPaths($this->config->get('system.plugins.path'), $this->plugin->name(), 'assets', $routeParams->get('type'), $routeParams->get('file'));
+
+        if (FileSystem::isFile($path, assertExists: false)) {
+            $headers = ($this->request->query()->has('v'))
+                ? ['Cache-Control' => 'private, max-age=31536000, immutable']
+                : [];
+            return new FileResponse($path, headers: $headers, autoEtag: true, autoLastModified: true);
+        }
+
+        return $this->forward(PageController::class, 'error');
+    }
+}
--- formwork/src/Plugins/Plugin.php
+++ formwork/src/Plugins/Plugin.php
@@ -4,6 +4,8 @@ namespace Formwork\Plugins;
 
 use Composer\Autoload\ClassLoader;
 use Formwork\Cms\App;
+use Formwork\Plugins\Controllers\AssetsController;
+use Formwork\Utils\FileSystem;
 use Formwork\Utils\Str;
 use Formwork\View\ViewFactory;
 
@@ -25,14 +27,45 @@ class Plugin
     }
 
     /**
+     * Get the plugin name
+     */
+    final public function name(): string
+    {
+        return basename($this->path());
+    }
+
+    /**
+     * Get the plugin namespace
+     */
+    final public function namespace(): string
+    {
+        return "plugin:{$this->name()}";
+    }
+
+    /**
      * Method called when the plugin is initialized
      */
     public function initialize(): void
     {
+        if (FileSystem::isDirectory($viewsPath = FileSystem::joinPaths($this->path(), 'views'), assertExists: false)) {
+            $this->viewFactory->setResolutionPaths([$this->namespace() => $viewsPath]);
+        }
+
+        if (FileSystem::isDirectory($assetsPath = FileSystem::joinPaths($this->path(), 'assets'), assertExists: false)) {
+            $this->app->assets()->setResolutionPaths([
+                $this->namespace() => [
+                    'path' => $assetsPath,
+                    'uri'  => $this->app->site()->uri("/plugins/{$this->name()}/assets", includeLanguage: false),
+                ],
+            ]);
+
+            $this->app->router()->addRoute("{$this->namespace()}.assets", "/plugins/{$this->name()}/assets/{type:alpha}/{file:all}")
+                ->methods('GET')
+                ->action(AssetsController::class . '@asset')
+                ->actionParameters(['plugin' => $this]);
+        }
+
         $this->initialized = true;
-        $this->viewFactory->setResolutionPaths([
-            'plugin:' . basename($this->path()) => $this->path() . '/views/',
-        ]);
     }
 
     /**
